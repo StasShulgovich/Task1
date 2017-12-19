851f3cbbbbdee1c10c29dd9b5a7da3a9f77052ab
Станислав Шульгович <Stas_shulgovich@mail.ru>
2017-12-20 00:18:55 +0300

    2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -21,26 +21,26 @@ namespace Task1
 
         private static void init()
         {
-            marks = new List<Mark> { new Mark("test0", new DateTime(1234567890), 0),
-                                     new Mark("test1", new DateTime(1234567890), 1),
-                                     new Mark("test2", new DateTime(1234567890), 2),
-                                     new Mark("test3", new DateTime(1234567890), 3),
-                                     new Mark("test4", new DateTime(1234567890), 4),
-                                     new Mark("test5", new DateTime(1234567890), 5),
-                                     new Mark("test6", new DateTime(1234567890), 6),
-                                     new Mark("test7", new DateTime(1234567890), 7),
-                                     new Mark("test8", new DateTime(1234567890), 8),
-                                     new Mark("test9", new DateTime(1234567890), 9) };
-            students = new List<Student> { new Student("test0", "test0", new DateTime(1234567890)),
-                                           new Student("test1", "test1", new DateTime(1234567890)),
-                                           new Student("test2", "test2", new DateTime(1234567890)),
-                                           new Student("test3", "test3", new DateTime(1234567890)),
-                                           new Student("test4", "test4", new DateTime(1234567890)),
-                                           new Student("test5", "test5", new DateTime(1234567890)),
-                                           new Student("test6", "test6", new DateTime(1234567890)),
-                                           new Student("test7", "test7", new DateTime(1234567890)),
-                                           new Student("test8", "test8", new DateTime(1234567890)),
-                                           new Student("test9", "test9", new DateTime(1234567890)) };
+            marks = new List<Mark> { new Mark("Math0", new DateTime(1234567890), 0),
+                                     new Mark("Literature1", new DateTime(1234567890), 1),
+                                     new Mark("Geography2", new DateTime(1234567890), 2),
+                                     new Mark("Biology3", new DateTime(1234567890), 3),
+                                     new Mark("Chemistry4", new DateTime(1234567890), 4),
+                                     new Mark("Physics5", new DateTime(1234567890), 5),
+                                     new Mark("Russhian6", new DateTime(1234567890), 6),
+                                     new Mark("English7", new DateTime(1234567890), 7),
+                                     new Mark("History8", new DateTime(1234567890), 8),
+                                     new Mark("Music9", new DateTime(1234567890), 9) };
+            students = new List<Student> { new Student("Name0", "LastName0", new DateTime(1234567890)),
+                                           new Student("Name1", "LastName1", new DateTime(1234567890)),
+                                           new Student("Name2", "LastName2", new DateTime(1234567890)),
+                                           new Student("Name3", "LastName3", new DateTime(1234567890)),
+                                           new Student("Name4", "LastName4", new DateTime(1234567890)),
+                                           new Student("Name5", "LastName5", new DateTime(1234567890)),
+                                           new Student("Name6", "LastName6", new DateTime(1234567890)),
+                                           new Student("Name7", "LastName7", new DateTime(1234567890)),
+                                           new Student("Name8", "LastName8", new DateTime(1234567890)),
+                                           new Student("Name9", "LastName9", new DateTime(1234567890)) };
             group = new StudentGroup();
             for (int i = 0; i < 10; i++)
             {
